0354c1873c77cb64e1bd221beaf217305cf02e1e
sssukh <sssukh50@gmail.com>
2021-07-19 21:12:49 +0900

    day11 chapter3 start

---
+++ chapter3/MoveComponent.h
@@ -0,0 +1,19 @@
+#pragma once
+class MoveComponent : public Component
+{
+public:
+	//먼저 업데이트 되도록 갱신 순서값을 낮춤
+	MoveComponent(class Actor* owner, int updateOrder = 10);
+
+	void Update(float deltaTime) override;
+
+	float GetAngularSpeed() const { return mAngularSpeed; }
+	float GetForwardSpeed() const { return mForwardSpeed; }
+	void SetAngularSpeed(float speed) { mAngularSpeed = speed; }
+	void SetForwardSpeed(float speed) { mForwardSpeed = speed; }
+private:
+	// 회전을 제어(초당 라디안)
+	float mAngularSpeed;
+	// 전방 이동을 제어(초당 단위)
+	float mForwardSpeed;
+};
---
+++ chapter3/소스.cpp
@@ -0,0 +1,18 @@
+#include "MoveComponent.h"
+
+
+MoveComponent::Update(float deltaTime)
+{
+	if (!Math::NearZeor(mAngularSpeed))
+	{
+		float rot = mOwner->GetRotation();
+		rot += mAngularSpeed * deltaTime;
+		mOwner->SetRotation(rot);
+	}
+	if (!Math::NearZero(mForwardSpeed))
+	{
+		Vector2 pos = mOwner->GetPosition();
+		pos += mOwner->GetForward() * mForwardSpeed * deltaTime;
+		mOwner->SetPosition(pos);
+	}
+}
